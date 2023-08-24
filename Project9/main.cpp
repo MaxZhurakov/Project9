f6e0ab9c1c1f17e91ab66b623b93248cd9c2be14
Zhura_is65 <muxmax969@gmail.com>
2023-08-24 16:36:00 +0300

    commit

--- Project9/main.cpp
+++ Project9/main.cpp
@@ -1,87 +1,36 @@
 //HM_ex_1_24.03.2023_
 #include <iostream>
 using namespace std;
-void compressArray(int arr[], int n) {
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        if (arr[i] != 0) {
-            arr[count] = arr[i];
-            count++;
-        }
-    }
-    while (count < n) {
-        arr[count] = -1;
-        count++;
-    }
+struct WashingMachine 
+{
+    string manufacturer;
+    string color;
+    double width;
+    double height;
+    int power;
+    int spin_speed;
+};
+void print_washer(WashingMachine washer) 
+{
+    cout << "Manufacturer: " << washer.manufacturer << endl;
+    cout << "Color: " << washer.color << endl;
+    cout << "Width: " << washer.width << " cm" << endl;
+    cout << "Height: " << washer.height << " cm" << endl;
+    cout << "Power: " << washer.power << " W" << endl;
+    cout << "Spin speed: " << washer.spin_speed << " rpm" << endl;
 }
-int main() {
-    int arr[] = { 1, 0, 2, 0, 3, 4, 0, 5 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+int main() 
+{
+    WashingMachine my_washer;
+    my_washer.manufacturer = "Samsung";
+    my_washer.color = "Black";
+    my_washer.width = 67.0;
+    my_washer.height = 89.0;
+    my_washer.power = 2400;
+    my_washer.spin_speed = 2000;
 
-    cout << "Original array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    cout << "My washing machine: " << endl;
+    print_washer(my_washer);
 
-    compressArray(arr, n);
-
-    cout << "Compressed array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
-//HM_ex_2_24.03.2023_
-#include <iostream>
-using namespace std;
-int main() {
-    int arr1[5] = { 3, 1, -2, 0, 4 };
-    int arr2[5] = { -1, 2, 0, -3, 5 };
-    int arr3[10];
-    int pos = 0, zero = 0, neg = 0;
-    for (int i = 0; i < 5; i++) {
-        if (arr1[i] > 0) {
-            arr3[pos] = arr1[i];
-            pos++;
-        }
-    }
-    for (int i = 0; i < 5; i++) {
-        if (arr2[i] > 0) {
-            arr3[pos] = arr2[i];
-            pos++;
-        }
-    }
-    for (int i = 0; i < 5; i++) {
-        if (arr1[i] == 0) {
-            arr3[pos + zero] = arr1[i];
-            zero++;
-        }
-    }
-    for (int i = 0; i < 5; i++) {
-        if (arr2[i] == 0) {
-            arr3[pos + zero] = arr2[i];
-            zero++;
-        }
-    }
-    for (int i = 0; i < 5; i++) {
-        if (arr1[i] < 0) {
-            arr3[pos + zero + neg] = arr1[i];
-            neg++;
-        }
-    }
-    for (int i = 0; i < 5; i++) {
-        if (arr2[i] < 0) {
-            arr3[pos + zero + neg] = arr2[i];
-            neg++;
-        }
-    }
-    cout << "Merged array: ";
-    for (int i = 0; i < 10; i++) {
-        cout << arr3[i] << " ";
-    }
-    cout << endl;
     return 0;
 }
