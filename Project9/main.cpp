deecdb629e9e2ac3d0c94b6775974da7af2b1d8e
Zhura_is65 <muxmax969@gmail.com>
2023-08-28 23:52:09 +0300

    commit

--- Project9/main.cpp
+++ Project9/main.cpp
@@ -1,220 +1,147 @@
-//HM_ex_1_24.03.2023_
+//HM_ex_1_7.05.2023
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
+
 using namespace std;
-// Клас, що представляє співробітника
-class Employee {
-public:
-    string surname;
-    string name;
-    int age;
-    Employee(const string& surname, const string& name, int age)
-        : surname(surname), name(name), age(age) {}
-};
-// Клас, що представляє базу даних співробітників
-class EmployeeDatabase 
+struct Rectangle 
 {
-private:
-    vector<Employee> employees;
-    string filename;
-public:
-    EmployeeDatabase(const string& filename)
-        : filename(filename) {}
-    // Додавання співробітника до бази даних
-    void addEmployee(const Employee& employee) 
-    {
-        employees.push_back(employee);
-    }
-    // Видалення співробітника з бази даних за прізвищем
-    void removeEmployee(const string& surname) 
-    {
-        employees.erase(remove_if(employees.begin(), employees.end(),
-            [&](const Employee& employee) { return employee.surname == surname; }), employees.end());
-    }
-    // Пошук співробітників за прізвищем
-    vector<Employee> searchBySurname(const string& surname) const 
-    {
-        vector<Employee> result;
-        for (const Employee& employee : employees) 
-        {
-            if (employee.surname == surname) 
-            {
-                result.push_back(employee);
-            }
-        }
-        return result;
-    }
-    // Пошук співробітників за віком
-    vector<Employee> searchByAge(int age) const 
-    {
-        vector<Employee> result;
-        for (const Employee& employee : employees) 
-        {
-            if (employee.age == age) {
-                result.push_back(employee);
-            }
-        }
-        return result;
-    }
-    // Пошук співробітників за першою літерою прізвища
-    vector<Employee> searchByFirstLetter(char letter) const 
-    {
-        vector<Employee> result;
-        for (const Employee& employee : employees) 
-        {
-            if (employee.surname[0] == letter) 
-            {
-                result.push_back(employee);
-            }
-        }
-        return result;
-    }
-    // Завантаження списку співробітників з файлу
-    void loadFromFile() 
-    {
-        ifstream file(filename);
-        if (file.is_open()) 
-        {
-            employees.clear();
-            string surname, name;
-            int age;
-            while (file >> surname >> name >> age) 
-            {
-                employees.emplace_back(surname, name, age);
-            }
-            file.close();
-        }
-    }
-    // Збереження списку співробітників у файл
-    void saveToFile() const 
-    {
-        ofstream file(filename);
-        if (file.is_open()) 
-        {
-            for (const Employee& employee : employees)
-            {
-                file << employee.surname << " " << employee.name << " " << employee.age << endl;
-            }
-            file.close();
-        }
-    }
+	double width;
+	double height;
+	double x;
+	double y;
 };
+
+void moveRectangle(Rectangle& rect, double offsetX, double offsetY) 
+{
+	rect.x += offsetX;
+	rect.y += offsetY;
+}
+
+
+void resizeRectangle(Rectangle& rect, double newWidth, double newHeight) 
+{
+	rect.width = newWidth;
+	rect.height = newHeight;
+}
+
+
+void printRectangle(const Rectangle& rect) 
+{
+	std::cout << "Rectangle Details:" << std::endl;
+	std::cout << "Width: " << rect.width << std::endl;
+	std::cout << "Height: " << rect.height << std::endl;
+	std::cout << "Position (x, y): (" << rect.x << ", " << rect.y << ")" << std::endl;
+}
+
+
 int main() 
 {
-    string filename = "employees.txt";
-    EmployeeDatabase database(filename);
-    // Завантаження списку співробітників з файлу
-    database.loadFromFile();
-    int choice;
-    do {
-        cout << "=== ІНФОРМАЦІЙНА СИСТЕМА \"СПІВРОБІТНИКИ\" ===" << endl;
-        cout << "1. Додати співробітника" << endl;
-        cout << "2. Видалити співробітника" << endl;
-        cout << "3. Пошук за прізвищем" << endl;
-        cout << "4. Пошук за віком" << endl;
-        cout << "5. Пошук за першою літерою прізвища" << endl;
-        cout << "6. Зберегти список співробітників у файл" << endl;
-        cout << "0. Вихід" << endl;
-        cout << "Ваш вибір: ";
-        cin >> choice;
-        switch (choice)
-        {
-        case 1: 
-        {
-            string surname, name;
-            int ag
-            cout << "Введіть прізвище: ";
-            cin >> surname;
-            cout << "Введіть ім'я: ";
-            cin >> name;
-            cout << "Введіть вік: ";
-            cin >> age;
-            database.addEmployee(Employee(surname, name, age));
-            cout << "Співробітник успішно доданий!" << endl;
-            break;
-        }
-        case 2:
-        {
-            string surname;
-            cout << "Введіть прізвище співробітника, якого бажаєте видалити: ";
-            cin >> surname;
-            database.removeEmployee(surname);
-            cout << "Співробітник успішно видалений!" << endl;
-            break;
-        }
-        case 3: 
-        {
-            string surname;
-            cout << "Введіть прізвище для пошуку: ";
-            cin >> surname;
-            vector<Employee> result = database.searchBySurname(surname);
-            if (result.empty()) {
-                cout << "Співробітників з таким прізвищем не знайдено." << endl;
-            }
-            else
-            {
-                cout << "Результати пошуку:" << endl;
-                for (const Employee& employee : result) {
-                    cout << "Прізвище: " << employee.surname << ", Ім'я: " << employee.name << ", Вік: " << employee.age << endl;
-                }
-            }
-            break;
-        }
-        case 4:
-        {
-            int age;
-            cout << "Введіть вік для пошуку: ";
-            cin >> age;
-            vector<Employee> result = database.searchByAge(age);
-            if (result.empty()) 
-            {
-                cout << "Співробітників з таким віком не знайдено." << endl;
-            }
-            else 
-            {
-                cout << "Результати пошуку:" << endl;
-                for (const Employee& employee : result) 
-                {
-                    cout << "Прізвище: " << employee.surname << ", Ім'я: " << employee.name << ", Вік: " << employee.age << endl;
-                }
-            }
-            break;
-        }
-        case 5: 
-        {
-            char letter;
-            cout << "Введіть літеру для пошуку: ";
-            cin >> letter;
-            vector<Employee> result = database.searchByFirstLetter(letter);
-            if (result.empty()) 
-            {
-                cout << "Співробітників з прізвищами, що починаються на задану літеру, не знайдено." << endl;
-            }
-            else 
-            {
-                cout << "Результати пошуку:" << endl;
-                for (const Employee& employee : result)
-                {
-                    cout << "Прізвище: " << employee.surname << ", Ім'я: " << employee.name << ", Вік: " << employee.age << endl;
-                }
-            }
-            break;
-        }
-        case 6: 
-        {
-            database.saveToFile();
-            cout << "Список співробітників успішно збережений у файл!" << endl;
-            break;
-        }
-        case 0:
-            break;
-        default:
-            cout << "Невірний вибір. Спробуйте ще раз." << endl;
-            break;
-        }
-        cout << endl;
-    } while (choice != 0);
-    return 0;
+    Rectangle rect;
+	rect.width = 5.0;
+	rect.height = 3.0;
+	rect.x = 2.0;
+	rect.y = 1.0;
+	printRectangle(rect);
+	moveRectangle(rect, 3.0, -2.0);
+	resizeRectangle(rect, 7.0, 4.0);
+	printRectangle(rect);
+	return 0;
+}
+//HM_ex_2_7.05.2023
+#include <iostream>
+#include <cmath>
+using namespace std;
+struct Point 
+{
+	double x;
+	double y;
+};
+double calculateDistance(const Point& p1, const Point& p2) 
+{
+	double distance = sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+	return distance;
+}
+int main() 
+{
+	Point p1, p2;
+	p1.x = 2.0;
+	p1.y = 3.0;
+	p2.x = -1.0;
+	p2.y = 5.0;
+	double distance = calculateDistance(p1, p2);
+	std::cout << "Distance between the two points: " << distance << std::endl;
+	return 0;
+}
+//HM_ex_3_7.05.2023
+#include <iostream>
+using namespace std;
+struct Fraction {
+	int numerator;   // чисельник
+	int denominator; // знаменник
+};
+// функція для знаходження найбільшого спільного дільника двох чисел
+int gcd(int a, int b)
+{
+	if (b == 0)
+	{
+		return a;
+	}
+	return gcd(b, a % b);
+}
+// функція для скорочення дробу
+void simplify(Fraction& f) 
+{
+	int divisor = gcd(f.numerator, f.denominator);
+	f.numerator /= divisor;
+	f.denominator /= divisor;
+}
+// функція для перетворення неправильного дробу на простий
+void toProper(Fraction& f) 
+{
+	if (f.numerator >= f.denominator) 
+	{
+		int whole = f.numerator / f.denominator;
+		f.numerator -= whole * f.denominator;
+		f.numerator %= f.denominator;
+		f.numerator += whole * f.denominator;
+	}
+}
+// функція для додавання двох дробів
+Fraction add(Fraction f1, Fraction f2)
+{
+	Fraction result;
+	result.numerator = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+	result.denominator = f1.denominator * f2.denominator;
+	simplify(result);
+	toProper(result);
+	return result;
+}
+// функція для віднімання двох дробів
+Fraction subtract(Fraction f1, Fraction f2) 
+{
+	Fraction result;
+	result.numerator = f1.numerator * f2.denominator - f2.numerator * f1.denominator;
+	result.denominator = f1.denominator * f2.denominator;
+	simplify(result);
+	toProper(result);
+	return result;
+}
+// функція для множення двох дробів
+Fraction multiply(Fraction f1, Fraction f2) 
+{
+	Fraction result;
+	result.numerator = f1.numerator * f2.numerator;
+	result.denominator = f1.denominator * f2.denominator;
+	simplify(result);
+	toProper(result);
+	return result;
+}
+// функція для ділення двох дробів
+Fraction divide(Fraction f1, Fraction f2) 
+{
+	Fraction result;
+	result.numerator = f1.numerator * f2.denominator;
+	result.denominator = f1.denominator * f2.numerator;
+	simplify(result);
+	toProper(result);
+	return result;
 }
