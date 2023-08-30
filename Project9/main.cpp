5d82f6354f9bed00d6aa32bd12434e7cc6d9aa8d
Zhura_is65 <muxmax969@gmail.com>
2023-08-30 22:07:40 +0300

    commit

--- Project9/main.cpp
+++ Project9/main.cpp
@@ -1,90 +1,66 @@
 //HM_ex_1_3.04.2023
 #include <iostream>
+#include <string>
 using namespace std;
-const int MAX_SIZE = 100;
-class SquareMatrix 
-{
-private:
-    int size;
-    double matrix[MAX_SIZE][MAX_SIZE];
-public:
-    void init(int n) 
-    {
-        size = n;
-        for (int i = 0; i < size; i++) 
-        {
-            for (int j = 0; j < size; j++) 
-            {
-                matrix[i][j] = 0;
-            }
-        }
-    }
-
-    void init(int n, double value) {
-        size = n;
-        for (int i = 0; i < size; i++) 
-        {
-            for (int j = 0; j < size; j++) 
-            {
-                matrix[i][j] = value;
-            }
-        }
-    }
-    void init(int n, char c) 
-    {
-        size = n;
-        for (int i = 0; i < size; i++) 
-        {
-            for (int j = 0; j < size; j++) 
-            {
-                if (i == j) {
-                    matrix[i][j] = c;
-                }
-                else {
-                    matrix[i][j] = ' ';
-                }
-            }
-        }
-    }
-    void print() {
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++) 
-            {
-                cout << matrix[i][j] << " ";
-            }
-            cout << endl;
-        }
-    }
-    double getMax() {
-        double max = matrix[0][0];
-        for (int i = 1; i < size; i++) 
-        {
-            if (matrix[i][i] > max) 
-            {
-                max = matrix[i][i];
-            }
-        }
-        return max;
-    }
-    double getMin() {
-        double min = matrix[0][0];
-        for (int i = 1; i < size; i++) 
-        {
-            if (matrix[i][i] < min) 
-            {
-                min = matrix[i][i];
-            }
-        }
-        return min;
-    }
-};
 int main()
 {
-    SquareMatrix matrix;
-    matrix.init(5, 'X');
-    matrix.print();
-    cout << "Max on diagonal: " << matrix.getMax() << endl;
-    cout << "Min on diagonal: " << matrix.getMin() << endl;
+    string text = "Rebrov didn't like that I called him a messiah for the national team, I think I'll survive it.";
+    string word;
+    int count = 0;
+    cout << "Enter word: ";
+    cin >> word;
+    size_t pos = text.find(word);
+    while (pos != string::npos)
+    {
+        count++;
+        pos = text.find(word, pos + 1);
+    }
+    cout << "The number of occurrences of the word '" << word << "' in the text: " << count << endl;
+    int sentence_count = 0;
+    for (size_t i = 0; i < text.length(); i++)
+    {
+        if (text[i] == '.' || text[i] == '?')
+        {
+            sentence_count++;
+        }
+    }
+    cout << "Number of sentences in the text: " << sentence_count << endl;
+    int dot_count = 0;
+    int comma_count = 0;
+    for (size_t i = 0; i < text.length(); i++)
+    {
+        if (text[i] == '.')
+        {
+            dot_count++;
+        }
+        else if (text[i] == ',')
+        {
+            comma_count++;
+        }
+    }
+    cout << "The number of points in the text: " << dot_count << endl;
+    cout << "The number of commas in the text: " << comma_count << endl;
+    string reversed_text;
+    for (int i = text.length() - 1; i >= 0; i--)
+    {
+        reversed_text += text[i];
+    }
+    cout << "Inverted text: " << reversed_text << endl;
+    string reversed_sentences;
+    string sentence;
+    for (size_t i = 0; i < text.length(); i++)
+    {
+        sentence += text[i];
+        if (text[i] == '.' || text[i] == '?')
+        {
+            for (int j = sentence.length() - 2; j >= 0; j--)
+            {
+                reversed_sentences += sentence[j];
+            }
+            reversed_sentences += sentence[sentence.length() - 1];
+            sentence = "";
+        }
+    }
+    cout << "Inverted sentences in the text: " << reversed_sentences << endl;
     return 0;
 }
